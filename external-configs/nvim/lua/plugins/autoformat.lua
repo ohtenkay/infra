c95c2c83d063ed5c27e7c04fd5ff5a1910f6e318
Ondrej Hlozek <hlozek.o1@gmail.com>
2026-04-02 03:53:38 +0200

    feat: java formatting

--- external-configs/nvim/lua/plugins/autoformat.lua
+++ external-configs/nvim/lua/plugins/autoformat.lua
@@ -13,23 +13,35 @@ return {
     },
   },
   ---@module "conform"
-  opts = {
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      typescript = { 'prettierd' },
-      javascript = { 'prettierd' },
-      javascriptreact = { 'prettierd' },
-      typescriptreact = { 'prettierd' },
-      css = { 'prettierd' },
-      scss = { 'prettierd' },
-      html = { 'prettierd' },
-      json = { 'prettierd' },
-      jsonc = { 'prettierd' },
-      svelte = { 'prettierd' },
-    },
-    default_format_opts = {
-      lsp_format = 'fallback',
-    },
-    format_on_save = { timeout_ms = 500 },
-  },
+  opts = function(_, opts)
+    local default_opts = {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        typescript = { 'prettierd' },
+        javascript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        css = { 'prettierd' },
+        scss = { 'prettierd' },
+        html = { 'prettierd' },
+        json = { 'prettierd' },
+        jsonc = { 'prettierd' },
+        svelte = { 'prettierd' },
+        java = { 'palantir-java-format' },
+      },
+      formatters = {
+        ['palantir-java-format'] = {
+          command = 'palantir-java-format',
+          args = { '--palantir', '--replace', '$FILENAME' },
+          stdin = false,
+        },
+      },
+      default_format_opts = {
+        lsp_format = 'fallback',
+      },
+      format_on_save = { timeout_ms = 1000 },
+    }
+
+    return vim.tbl_deep_extend('force', default_opts, opts or {})
+  end,
 }
--- modules/dev/java.nix
+++ modules/dev/java.nix
@@ -3,6 +3,38 @@
   flake.modules.nixos.dev =
     { pkgs, ... }:
     let
+      palantirJavaFormatVersion = "2.90.0";
+      palantirJavaFormatNative =
+        if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then
+          {
+            url = "https://repo1.maven.org/maven2/com/palantir/javaformat/palantir-java-format-native/${palantirJavaFormatVersion}/palantir-java-format-native-${palantirJavaFormatVersion}-nativeImage-linux-glibc_x86-64.bin";
+            hash = "sha256-BU3rzDRU/LjFE1Jn0O0GkK1HTaNvXufJSuqWZQh2mdE=";
+          }
+        else if pkgs.stdenv.hostPlatform.system == "aarch64-linux" then
+          {
+            url = "https://repo1.maven.org/maven2/com/palantir/javaformat/palantir-java-format-native/${palantirJavaFormatVersion}/palantir-java-format-native-${palantirJavaFormatVersion}-nativeImage-linux-glibc_aarch64.bin";
+            hash = "sha256-kDWDDvHflvki0Vxt4RaO0Gz2dcqfSolKpVKEKDT9iGA=";
+          }
+        else
+          throw "palantir-java-format is only configured for Linux x86_64/aarch64";
+
+      palantirJavaFormat = pkgs.stdenvNoCC.mkDerivation {
+        pname = "palantir-java-format";
+        version = palantirJavaFormatVersion;
+        src = pkgs.fetchurl palantirJavaFormatNative;
+        dontUnpack = true;
+        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+        buildInputs = [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.zlib
+        ];
+        installPhase = ''
+          runHook preInstall
+          install -Dm755 "$src" "$out/bin/palantir-java-format"
+          runHook postInstall
+        '';
+      };
+
       mavenWithJDK25 = pkgs.writeShellScriptBin "mvn" ''
         export JAVA_HOME=${pkgs.jdk25}
         export PATH="$JAVA_HOME/bin:$PATH"
@@ -13,8 +45,9 @@
       environment.systemPackages = with pkgs; [
         jdk25
         mavenWithJDK25
-        jdt-language-server
+        (jdt-language-server.override { jdk = pkgs.jdk25; })
         lombok
+        palantirJavaFormat
       ];
     };
 }
