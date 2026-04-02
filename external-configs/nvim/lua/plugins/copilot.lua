23ba0dffae2281ace268ceee571e7f0a7f20791b
Ondrej Hlozek <hlozek.o1@gmail.com>
2026-04-02 03:54:07 +0200

    feat: copilot nes

--- external-configs/nvim/lua/plugins/copilot.lua
+++ external-configs/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,12 @@
 return {
   {
     'zbirenbaum/copilot.lua',
+    dependencies = {
+      'copilotlsp-nvim/copilot-lsp',
+      init = function()
+        vim.g.copilot_nes_debounce = 500
+      end,
+    },
     event = 'InsertEnter',
     cmd = 'Copilot',
     opts = {
@@ -12,6 +18,14 @@ return {
         keymap = {
           accept = '<M-L>',
           accept_word = '<M-l>',
+        },
+      },
+      nes = {
+        enabled = true,
+        keymap = {
+          accept_and_goto = '<M-L>',
+          accept = false,
+          dismiss = '<Esc>',
         },
       },
       filetypes = {
